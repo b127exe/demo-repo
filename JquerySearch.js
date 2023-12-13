9721fbdedaede86ed39c3f9bd3207db6b4645da0
b127exe <bilalimran6217523@gmail.com>
2023-12-13 11:30:16 +0500

    third commit

--- JquerySearch.js
+++ JquerySearch.js
@@ -3,7 +3,7 @@ $("#txtSearch").keyup(function(){
 	$("tbody tr").each(function(){
 		if($(this).text().search(new RegExp(typeValue,"i")) < 0){
 			$(this).fadeOut();
-			console.log("world");
+			console.log("world third time");
 		} 
 		else{
 			$(this).show();
